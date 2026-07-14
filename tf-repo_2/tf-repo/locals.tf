71e160e65a8c6798c276c60fe5d50837c101c914
Amit Kumar <142219533+Akay-dev35@users.noreply.github.com>
2026-07-14 10:57:05 +0530

    Clean up comments in locals.tf
    Removed comments explaining resource group defaults.

--- tf-repo_2/tf-repo/locals.tf
+++ tf-repo_2/tf-repo/locals.tf
@@ -1,6 +1,4 @@
 locals {
-  # Each resource group only needs to specify what's *different*
-  # from the defaults below. Anything omitted falls back automatically.
   resource_groups = {
     for name, rg in var.resource_groups : name => {
       location = coalesce(rg.location, var.default_location)
